55df9612d75f7c9c0a382f27cefb78f5140dcca2
Jasper A. Visser <jasper.a.visser@gmail.com>
2015-09-20 00:58:34 +0200

    Rename SVM trainer file

---
+++ src/main/scala/tom/kaggle/springleaf/ml/SvmTrainer.scala
@@ -0,0 +1,25 @@
+package tom.kaggle.springleaf.ml
+
+import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.feature.PCA
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.rdd.RDD
+
+case class SvmTrainer(trainingDataset: RDD[LabeledPoint], components: Int, numIterations: Int) {
+
+  lazy val model = {
+    println(s"Training model on ${reducedFeatures.count()} instances")
+    SVMWithSGD.train(reducedFeatures, numIterations)
+      .clearThreshold()
+  }
+
+  private lazy val pca = {
+    println("Fitting PCA model")
+    new PCA(components).fit(trainingDataset.map(_.features))
+  }
+
+  private lazy val reducedFeatures: RDD[LabeledPoint] = {
+    println("Reducing dataset")
+    trainingDataset.map(p => p.copy(features = pca.transform(p.features))).cache()
+  }
+}
