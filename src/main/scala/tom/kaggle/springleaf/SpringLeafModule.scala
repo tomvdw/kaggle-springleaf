ae75d8b8db3124db3cc529f9e13d98c4c6472cba
Jasper Visser <j.visser@portavita.eu>
2015-09-24 14:09:44 +0200

    Fix broken injection of some variables

--- src/main/scala/tom/kaggle/springleaf/SpringLeafModule.scala
+++ src/main/scala/tom/kaggle/springleaf/SpringLeafModule.scala
@@ -5,23 +5,21 @@ import scaldi.Module
 import tom.kaggle.springleaf.analysis.{ColumnTypeInference, DataStatistics, RedisCacheAnalysis}
 
 class SpringLeafModule extends Module {
-  private val fraction = inject[Double]("data.fraction")
-  private val dataPath = inject[String]("data.path.base")
 
   binding to new RedisClient(
     host = inject[String]("redis.host"),
     port = inject[Int]("redis.port")
   )
-  binding to injected[KeyHelper]('fraction -> fraction)
+  binding to injected[KeyHelper]('fraction -> inject[Double]("data.fraction"))
   binding to injected[RedisCacheAnalysis]
   binding to injected[DataStatistics]('table -> Names.TableName)
   binding to new ColumnTypeInference
-  binding to injected[DataImporter]('dataPath -> dataPath, 'fraction -> fraction)
+  binding to injected[DataImporter]('dataPath -> inject[String]("data.path.base"), 'fraction -> inject[Double]("data.fraction"))
   binding identifiedBy "everything" to {
     val result = inject[DataImporter].readSample
     result.registerTempTable(Names.TableName)
     result
   }
   binding to injected[DataPreProcessor]
-  binding to injected[CategoricToIndexTransformer]('dataPath -> dataPath, 'fraction -> fraction)
+  binding to injected[CategoricToIndexTransformer]('dataPath -> inject[String]("data.path.base"), 'fraction -> inject[Double]("data.fraction"))
 }
